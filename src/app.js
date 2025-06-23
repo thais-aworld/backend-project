4365b7c1e2fd4f7902c7c986c8188de351f509e1
Seu Nome de Usuário <amorimthaissouza@gmail.com>
2025-06-23 13:06:41 -0300

    Atualizando rotas...

--- src/app.js
+++ src/app.js
@@ -1,34 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-
-dotenv.config();
-
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use('/v1', userRoutes);
-app.use('/v1', categoryRoutes);
-app.use('/v1', productRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API Ecommerce Project rodando 🚀' });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Rota não encontrada' });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Erro interno no servidor' });
 });
 
 module.exports = app;
